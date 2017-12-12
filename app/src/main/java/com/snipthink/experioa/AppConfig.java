8180ad09b60f766725cb6b01a58eb5308237df41
akjhaa007 <akjhaa007@gmail.com>
2017-12-12 21:21:24 +0530

    Initial commit

---
+++ app/src/main/java/com/snipthink/experioa/AppConfig.java
@@ -0,0 +1,13 @@
+package com.snipthink.experioa;
+
+/**
+ * Created by SnipThinK on 11/5/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.13/Serverr/include/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.13/Serverr/include/register.php";
+}
